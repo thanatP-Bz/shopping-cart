2ef9842be24e2d55be28c5aece53a448d4a571f7
Thanat <bztzn512@gmail.com>
2023-10-08 10:17:08 -0400

    addCanvas

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,11 @@
+import { Offcanvas } from "react-bootstrap";
+
+export const ShoppingCart = () => {
+  return (
+    <Offcanvas>
+      <Offcanvas.Header>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+    </Offcanvas>
+  );
+};
